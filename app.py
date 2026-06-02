84fc1fe3d0318df6a9f73f0982103b2b3cab0a35
Amalsaju13 <92660324+Amalsaju13@users.noreply.github.com>
2026-06-02 17:19:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ tab1, tab2, tab3, tab4 = st.tabs(
     [
         "Scenario Input",
         "Bus Timetable 🗓️",
-        "Station Queues",
+        "Station Queues ⏳",
         "Metrics"
     ]
 )
